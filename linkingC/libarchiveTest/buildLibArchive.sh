28528f0cf82df830889369c211a8d20999f0f967
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-05-13 11:25:23 +0200

    Switched off unneccessary stuff in the libArchive compilation.

--- linkingC/libarchiveTest/buildLibArchive.sh
+++ linkingC/libarchiveTest/buildLibArchive.sh
@@ -4,6 +4,7 @@ rm -rf libarchive_build
 mkdir libarchive_build
 cd libarchive_build
 
-cmake ../libarchive
+#cmake ../libarchive
+cmake -DENABLE_BZip2=OFF -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF ../libarchive
 make -j4
 make -j4 test
