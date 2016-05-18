a5bb27c7226181f0810cbe2ec9942df708c23264
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-05-18 13:56:33 +0200

    Factorization: Move the wrapper functions into a separate .c/.h file

--- linkingC/libarchiveTest/buildTest.sh
+++ linkingC/libarchiveTest/buildTest.sh
@@ -7,13 +7,17 @@ else
     echo "LibArchive already exists"
 fi
 
+#Build the wrapper
+echo "Building libArchive_wrapper.o..."
+gcc -Wall -g -Ilibarchive/libarchive/ -c libArchive_wrapper.c
+
 echo
 echo "*****************************************************************"
 echo
 
 #Build static libarchive:
 echo "Building test_libarchive_static..."
-gcc -static -Wall -g test_libarchive.c -Ilibarchive/libarchive/ libarchive_build/libarchive/libarchive.a -lz -lpthread -o test_libarchive_static
+gcc -static -Wall -g test_libarchive.c libArchive_wrapper.o -Ilibarchive/libarchive/ libarchive_build/libarchive/libarchive.a -lz -lpthread -o test_libarchive_static
 ./test_libarchive_static
 
 echo
@@ -22,7 +26,7 @@ echo
 
 #Build dynamic libarchive:
 echo "Building test_libarchive_dynamic..."
-gcc -Wall -g test_libarchive.c -Ilibarchive/libarchive/ -Llibarchive_build/libarchive/ -larchive -lz -o test_libarchive_dynamic
+gcc -Wall -g test_libarchive.c libArchive_wrapper.o -Ilibarchive/libarchive/ -Llibarchive_build/libarchive/ -larchive -lz -o test_libarchive_dynamic
 #echo "Done, to run, use"
 #echo "LD_LIBRARY_PATH=libarchive_build/libarchive/ ./test_libarchive_dynamic"
 LD_LIBRARY_PATH=libarchive_build/libarchive/ ./test_libarchive_dynamic
